8e5258b64559686451a1224726736abdd1144b64
Anna A <adobamena@gmail.com>
2025-10-08 23:06:57 -0400

    first way to add object to spring context

---
+++ sq-ch2-ex1/src/main/java/org/example/ProjectConfig.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    Parrot parrot(){
+        Parrot p = new Parrot();
+        p.setName("Koko");
+        return p;
+    }
+
+}
